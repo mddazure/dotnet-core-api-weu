8e6c07010a44fb93df108aede327f59dc2bcc3f8
mddazure <mddazure@gmail.com>
2020-11-27 17:19:30 +0100

    first commit

---
+++ wwwroot/app/scripts/todoListSvc.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+angular.module('todoApp')
+.factory('todoListSvc', ['$http', function ($http) {
+
+    $http.defaults.useXDomain = true;
+    delete $http.defaults.headers.common['X-Requested-With']; 
+
+    return {
+        getItems : function(){
+            return $http.get(apiEndpoint + 'weu/api/Todo');
+        },
+        getItem : function(id){
+            return $http.get(apiEndpoint + 'weu/api/Todo/' + id);
+        },
+        postItem : function(item){
+            return $http.post(apiEndpoint + 'weu/api/Todo', item);
+        },
+        putItem : function(item){
+            return $http.put(apiEndpoint + 'weu/api/Todo/' + item.id, item);
+        },
+        deleteItem : function(id){
+            return $http({
+                method: 'DELETE',
+                url: apiEndpoint + 'weu/api/Todo/' + id
+            });
+        }
+    };
+}]);
